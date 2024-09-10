c353820813999db4cb8b0b95ff95be10f75803a1
andres vallejo varela <andres4513330@gmail.com>
2024-09-09 21:28:23 -0500

    Guardamos avance

--- Ejercicio1_Coordenadas/Ejercicio1_Coordenadas/Program.cs
+++ Ejercicio1_Coordenadas/Ejercicio1_Coordenadas/Program.cs
@@ -1,7 +1,22 @@
 ﻿public class Program
 {
+    // Lea las coordenadas de dos puntos y que muestre la distancia entre dos puntos.
+
+    /*
+     * bloque
+     */
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        //Datos de entrada
+        //Declarar las variables
+
+        double x1, x2, y1, y2, d;
+
+        //Vamos a inicializar mis variables pidiendoles sus valores por pantalla:
+        Console.Write("Porfavor ingrese el valor de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine()); //Forma de escribir en pantalla
+
     }
 }
